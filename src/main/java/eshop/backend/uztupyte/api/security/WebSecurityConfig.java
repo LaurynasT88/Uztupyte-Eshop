71b3a8a31b4b3e08555e622a2a05f8fa20e361f1
LaurynasT88 <Laurynast1988@gmail.com>
2025-01-21 17:45:31 +0200

    Web security config added

--- src/main/java/eshop/backend/uztupyte/api/security/WebSecurityConfig.java
+++ src/main/java/eshop/backend/uztupyte/api/security/WebSecurityConfig.java
@@ -2,18 +2,39 @@ package eshop.backend.uztupyte.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 public class WebSecurityConfig {
 
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()) // Disable CSRF
+                .cors(cors -> cors.disable()) // Disable CORS
+                .addFilterBefore(jwtRequestFilter, org.springframework.security.web.access.intercept.AuthorizationFilter.class) // Ensure the JWT filter is applied before AuthorizationFilter
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/product", "/auth/register", "/auth/login").permitAll() // Allow unauthenticated access to these endpoints
+                        .anyRequest().authenticated() // Require authentication for all other requests
+                );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
