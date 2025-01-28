00d030e1a8dcf847bebf4e0115ef3d18ee596be2
LaurynasT88 <Laurynast1988@gmail.com>
2025-01-28 10:24:59 +0200

    test update

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,3 @@
--- Passwords are in the format : Password<UserLetter>123. Unless specified otherwise
-INSERT INTO customer (EMAIL, FIRST_NAME, LAST_NAME, PASSWORD, USERNAME, EMAIL_VERIFIED)
-VALUES
-    ('UserA@junit.com', 'UserA-first_name', 'UserA-last_name', '$2a$10$xfVdHnGjwpyTg3T4KW2DHuqAHJdhCeY2QvlMi3ukp5gIfw9N4zJ42', 'UserA', true),
-    ('UserB@junit.com', 'UserB-first_name', 'UserB-last_name', '$2a$10$sqK7Qce7wLN6ZJvVN3lwW.eZOsSJv3ZWWo0UScieolhOqI8pdbEqi', 'UserB', false);
+INSERT INTO customer (email, first_name, last_name, password, username, email_verified)
+VALUES ('UserA@junit.com', 'UserA-FirstName', 'UserA-LastName', '$2a$10$hBn5gu6cGelJNiE6DDsaBOmZgyumCSzVwrOK/37FWgJ6aLIdZSSI2', 'UserA', true)
+     , ('UserB@junit.com', 'UserB-FirstName', 'UserB-LastName', '$2a$10$TlYbg57fqOy/1LJjispkjuSIvFJXbh3fy0J9fvHnCpuntZOITAjVG', 'UserB', false);
