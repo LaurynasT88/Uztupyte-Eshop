2e3a2e0fadaaabbe80371720d770a9a6f26acc62
LaurynasT88 <Laurynast1988@gmail.com>
2025-02-18 19:49:43 +0200

    added s3 secrets

--- src/main/java/eshop/backend/uztupyte/config/S3Config.java
+++ src/main/java/eshop/backend/uztupyte/config/S3Config.java
@@ -4,6 +4,8 @@ import eshop.backend.uztupyte.service.LocalS3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -16,6 +18,13 @@ public class S3Config {
     @Value("${aws.s3.mock}")
     private boolean mock;
 
+    @Value("${aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+
     @Bean
     public S3Client s3Client() {
 
@@ -23,8 +32,14 @@ public class S3Config {
             return new LocalS3();
         }
 
+        AwsBasicCredentials credentials = AwsBasicCredentials.builder()
+                .accessKeyId(accessKey)
+                .secretAccessKey(secretKey)
+                .build();
+
         return S3Client.builder()
                 .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 
